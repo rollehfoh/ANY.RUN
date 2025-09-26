da570db9300dcd3ae541dc35f5f677f8de50e9a4
Semen Shalnev <85073445+semayellow@users.noreply.github.com>
2025-09-26 10:31:06 +0300

    feat(general): add source files

---
+++ connectors/Microsoft/Microsoft Defender for Endpoint/ANYRUN-TI-Feeds-MDE/src/ANYRUN-Feeds-MDE-FA/__init__.py
@@ -0,0 +1,243 @@
+{
+  "IsAvailableForCommunity": false,
+  "Identifier": "ANYRUN TI Lookup",
+  "ShouldInstalledInSystem": false,
+  "Version": 1.0,
+  "DisplayName": "ANYRUN TI Lookup",
+  "Description": "The ANY.RUN Threat Intelligence Feed connector provides Google SecOps users with a continuously updated stream of fresh, accurate indicators of compromise, including malicious IPs, URLs, and domains. This enables security teams to proactively defend against emerging, evolving, and persistent cyber threats.\nTo use the integration, ensure you have an active ANY.RUN TI Feed subscription",
+  "ImageBase64": null,
+  "SvgImage": "<svg width=\"100%\" height=\"100%\" viewBox=\"0 0 25 25\" fill=\"currentColor\" xmlns=\"http://www.w3.org/2000/svg\" aria-hidden=\"true\"> \t<path d=\"M6.61665 1.07387C6.44745 1.14638 6.22991 1.26724 6.13323 1.36392C5.69815 1.75066 5.72232 1.29141 5.72232 9.96877C5.72232 14.3679 5.74649 17.9935 5.79484 17.9935C5.81901 17.9935 6.1574 17.8002 6.54413 17.5826L7.22092 17.1717L7.24509 9.9446C7.24509 5.9564 7.26926 2.71749 7.29343 2.71749C7.3176 2.71749 20.2007 10.1621 20.4908 10.3313C20.5391 10.3555 20.2732 10.5489 19.9107 10.7422C19.5481 10.9598 19.258 11.1531 19.2822 11.1773C19.3064 11.2015 19.6448 11.419 20.0073 11.6366L20.7083 12.0233L20.95 11.8783C21.6751 11.4915 22.0135 11.2015 22.1586 10.8389C22.3761 10.3313 22.2794 9.84792 21.9168 9.46118C21.7235 9.26782 19.5239 7.93842 14.7623 5.2071C10.9916 3.08006 7.80102 1.24307 7.68017 1.17055C7.3176 0.977187 6.93087 0.953016 6.61665 1.07387Z\"></path> \t<path d=\"M2.9185 3.17674C2.5801 3.27343 2.14503 3.7085 2.04834 4.07107C2 4.2886 2 7.01992 2 12.6517L2.02417 20.9182L2.24171 21.2566C2.50759 21.6675 3.03935 21.9334 3.47443 21.8609C3.74031 21.8125 5.4081 20.894 10.7015 17.8243C14.4964 15.6489 17.6144 13.8361 17.6144 13.8361C17.6144 13.8361 17.276 13.6186 16.8651 13.3769L16.0917 12.9418L9.87972 16.5191C6.47162 18.5011 3.61945 20.1206 3.59528 20.1206C3.54694 20.1206 3.52277 17.0509 3.52277 12.4826C3.52277 7.89007 3.54694 4.84454 3.59528 4.84454C3.64362 4.84454 3.93367 4.98956 4.2479 5.18293C4.56212 5.3763 4.85217 5.52132 4.87634 5.52132C4.90051 5.52132 4.90051 5.11042 4.90051 4.627L4.87634 3.73267L4.22373 3.41845C3.57111 3.08006 3.37774 3.05589 2.9185 3.17674Z\"></path> \t<path d=\"M8.04273 6.51233L8.0669 7.40665L20.5391 14.6096L20.2974 14.7546C19.7898 15.0688 7.3176 22.2718 7.29343 22.2718C7.26926 22.2718 7.26926 21.9334 7.26926 21.4983C7.26926 21.0874 7.24509 20.749 7.19675 20.749C7.17258 20.749 6.83419 20.9665 6.42328 21.1841L5.72232 21.595V22.3201C5.72232 23.1178 5.79484 23.3595 6.18157 23.6737C6.51996 23.9637 6.95504 24.0604 7.3176 23.9637C7.55931 23.8912 20.5391 16.4708 21.6026 15.7698C22.0619 15.4797 22.2552 15.1414 22.2552 14.6096C22.2552 13.7878 22.642 14.0295 15.1248 9.70289C11.4025 7.55168 8.28444 5.76303 8.18776 5.71469L7.99439 5.61801L8.04273 6.51233Z\"></path> </svg>",
+  "IsCustom": true,
+  "PythonVersion": 3,
+  "IntegrationProperties": [
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Google service account",
+      "PropertyDescription": "Google service account info in raw json representation",
+      "PropertyDisplayName": "Google service account",
+      "PropertyType": 3,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "ANY.RUN Sandbox API KEY",
+      "PropertyDescription": "Example: API-KEY AJ13....KA!7",
+      "PropertyDisplayName": "ANY.RUN Sandbox API KEY",
+      "PropertyType": 3,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "ANY.RUN TI Feeds Basic token",
+      "PropertyDescription": "Example: Basic с2vtio5fbl...l0RvUag==",
+      "PropertyDisplayName": "ANY.RUN TI Feeds Basic token",
+      "PropertyType": 3,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "ANY.RUN TI Lookup API KEY",
+      "PropertyDescription": "Example: API-KEY AJ13....KA!7",
+      "PropertyDisplayName": "ANY.RUN TI Lookup API KEY",
+      "PropertyType": 3,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Project ID",
+      "PropertyDescription": "Google SecOps project ID",
+      "PropertyDisplayName": "Project ID",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Project location",
+      "PropertyDescription": "Google API project location",
+      "PropertyDisplayName": "Project location",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Instance ID",
+      "PropertyDescription": "Google API instance ID",
+      "PropertyDisplayName": "Instance ID",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Enable proxy",
+      "PropertyDescription": "Enable/disable proxy using",
+      "PropertyDisplayName": "Enable proxy",
+      "PropertyType": 0,
+      "IsMandatory": false,
+      "Value": "false",
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Proxy host",
+      "PropertyDescription": null,
+      "PropertyDisplayName": "Proxy host",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Proxy port",
+      "PropertyDescription": null,
+      "PropertyDisplayName": "Proxy port",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Enable proxy auth",
+      "PropertyDescription": "Enable/disable proxy authentication",
+      "PropertyDisplayName": "Enable proxy auth",
+      "PropertyType": 0,
+      "IsMandatory": false,
+      "Value": "false",
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Proxy username",
+      "PropertyDescription": null,
+      "PropertyDisplayName": "Proxy username",
+      "PropertyType": 2,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    },
+    {
+      "IntegrationIdentifier": "ANYRUN TI Lookup",
+      "PropertyName": "Proxy password",
+      "PropertyDescription": null,
+      "PropertyDisplayName": "Proxy password",
+      "PropertyType": 3,
+      "IsMandatory": false,
+      "Value": null,
+      "Id": 0,
+      "CreationTimeUnixTimeInMs": 1758264239618,
+      "ModificationTimeUnixTimeInMs": 1758264239618
+    }
+  ],
+  "IntegrationSupportedActions": [
+    {
+      "Name": "URL Android Analysis",
+      "Description": "",
+      "Id": 503,
+      "CreationTimeUnixTimeInMs": 1758545606203,
+      "ModificationTimeUnixTimeInMs": 1758545606206
+    },
+    {
+      "Name": "File Windows Analysis",
+      "Description": "",
+      "Id": 496,
+      "CreationTimeUnixTimeInMs": 1758545733826,
+      "ModificationTimeUnixTimeInMs": 1758545733828
+    },
+    {
+      "Name": "URL Linux Analysis",
+      "Description": "",
+      "Id": 502,
+      "CreationTimeUnixTimeInMs": 1758548494244,
+      "ModificationTimeUnixTimeInMs": 1758548494246
+    },
+    {
+      "Name": "File Linux Analysis",
+      "Description": "",
+      "Id": 499,
+      "CreationTimeUnixTimeInMs": 1758545756177,
+      "ModificationTimeUnixTimeInMs": 1758545756179
+    },
+    {
+      "Name": "File Android Analysis",
+      "Description": "",
+      "Id": 500,
+      "CreationTimeUnixTimeInMs": 1758535959299,
+      "ModificationTimeUnixTimeInMs": 1758535959302
+    },
+    {
+      "Name": "URL Windows Analysis",
+      "Description": "",
+      "Id": 501,
+      "CreationTimeUnixTimeInMs": 1758548032574,
+      "ModificationTimeUnixTimeInMs": 1758548032576
+    },
+    {
+      "Name": "Ping",
+      "Description": "Test Connectivity",
+      "Id": 280,
+      "CreationTimeUnixTimeInMs": 1758271332534,
+      "ModificationTimeUnixTimeInMs": 1758271332537
+    },
+    {
+      "Name": "ANYRUN TI Lookup",
+      "Description": "",
+      "Id": 504,
+      "CreationTimeUnixTimeInMs": 1758784521758,
+      "ModificationTimeUnixTimeInMs": 1758784521761
+    }
+  ],
+  "Categories": [],
+  "HasConnectors": false,
+  "DocumentationLink": null,
+  "IsCertified": false,
+  "UploaderFullName": "pro100bro",
+  "PublishComment": null,
+  "UploaderEmail": "cematheslayerforever@gmail.com",
+  "UploaderImageBase64": "",
+  "UploaderProfileLink": null,
+  "MinimumSystemVersion": 0.0,
+  "ProductionIntegrationIdentifier": null,
+  "Type": 0,
+  "Id": 0,
+  "CreationTimeUnixTimeInMs": 1758264239618,
+  "ModificationTimeUnixTimeInMs": 1758536245606
+}
